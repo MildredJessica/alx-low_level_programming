cff727a50aae616b62ff9b20e9d8c041788998fc
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-08 21:29:10 -0800

    Updated the 101-quotes file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-	write(STDOUT_FILENO, msg , 59);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, msg, 59);
 	return (1);
 }
