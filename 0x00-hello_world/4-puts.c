8975d5c2ab11ae7ea8f18affc7ac3ae03f9482cb
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-08 18:08:57 -0800

    Updated my 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 /**
  *
  * main - Entry point
+ * Prints exactly \"Programming is like building a multilingual puzzle
  *
  * Return: Always success (0)
  */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str1);
 	return (0);
 }
