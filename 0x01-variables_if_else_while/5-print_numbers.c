110274d0bd7031c69de4546fa846335d462d6d07
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-11 04:20:21 -0800

    Prints all the numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints all the numbers in base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+	{
+		printf("%d",num);
+	}
+	printf("\n");
+	return (0);
+}
