b7b85369b7e17a945a63b2078def8aeaa2f66d2a
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-13 20:05:24 -0800

    Changed the 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
 	return (0);
 }
