20c905fc296218768d6ff5f39e278cf3b227885a
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 03:28:30 -0800

    Changed the sign file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,17 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		_putchar('-' + '0');
+		_putchar('1');
 		return (-1);
 	} else if (n > 0)
 	{
 		_putchar('+' + '0');
+		_putchar('1');
 		return (1);
 	} else if (n == 0)
 	{
 		_putchar('0' + '0');
+		_putchar('0');
 		return (0);
 	}
 	return (0);
