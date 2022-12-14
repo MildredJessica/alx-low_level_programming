6cb9d2585174dc8546ec859b063d1673fdc56bc3
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 04:21:10 -0800

    Changed the print last digit file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * print_last_digit - Prints the digit of a number
  * @d: Parameter
- * Prints the last digit of f
+ * Prints the last digit of d
  * Return: the last digit
  */
 int print_last_digit(int d)
 {
-	int num = d % 10;
+	int a = abs(d);
+	int num = a % 10;
 
 	_putchar(num + '0');
 	return (num);
