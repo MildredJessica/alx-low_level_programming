466dd4cfa84cfdf1d9f953df8ca1ee8e5cf85b10
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 05:40:39 -0800

    Prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,20 @@ void times_table(void)
 	{
 		for (b = 0 ; b <= 9; b++)
 		{
-			_putchar((a * b) + '0');
-			_putchar(',');
-			_putchar(' ');
+			int d = a * b;
+
+			if (d > 9)
+			{
+				_putchar((d / 10) + '0');
+				_putchar((d % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			} else
+			{
+				_putchar(d + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('$');
 		_putchar('\n');
