078f846002a261cff734ab9bcf4c1d733c2bc969
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 00:23:17 -0800

    Checks if a character is an alphabet or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Checks whether a characetr is an alphabet
- * @c - Argument
+ * @c: Parameter
  * Checks whether c is an alphabet
  * Return: 1 if c is a letter, lowercase or uppercase, otherwise 0
  */
@@ -10,5 +10,6 @@ int _isalpha(int c)
 {
 	if ((c >= 'A' && c  <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
-
+	else
+		return (0);
 }
