3bf82825c08281efccd8724623b927783b77a21e
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 05:02:24 -0800

    Prints every hour of every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * jack_bauer - Prints all the minutes of the day
  * Return: Nothing
  */
 void jack_bauer(void)
 {
-	int num1 = 0;
 	int minutes, hour;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		int h = num1 + hour;
-
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
-			int min = num1 + minutes;
-
-			_putchar(h + '0');
-			_putchar(':');
-			_putchar(min + '0');
+			if (hour > 9 || minutes > 9)
+			{
+				printf("%d0", hour);
+				_putchar(':');
+				printf("%d0", minutes);
+			} else
+			{
+				printf("%d", hour);
+				_putchar(':');
+				printf("%d", minutes);
+			}
 			_putchar('\n');
 		}
 	}
