b0b8ff16410b5828ab0f4dba16721afecbf44ca9
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 19:15:35 -0800

    Prints numbers up to n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * print_to_98 - Print numbers from n to 98
  * @n: Parameter
@@ -11,8 +12,17 @@ void print_to_98(int n)
 	{
 		while (n != 98)
 		{
+			if (n < 0)
+			{
+				n = abs(n);
+				_putchar('-');
+			}
 			if ((n / 10) > 0)
 			{
+				if (n > 99)
+				{
+					_putchar((n / 10) + '0');
+				}
 				_putchar((n / 10) + '0');
 			}
 			_putchar((n % 10) + '0');
