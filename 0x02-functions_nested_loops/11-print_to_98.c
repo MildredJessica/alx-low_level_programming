7e5576b3304dad3b93a89913f3c33b330737814d
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-14 19:04:21 -0800

    Prints numbers up to n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_to_98 - Print numbers from n to 98
+ * @n: Parameter
+ * Prints numbers from n to 98
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n != 98)
+		{
+			if ((n / 10) > 0)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	} else if (n > 98)
+	{
+		while (n != 98)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
+	_putchar('\n');
+
+}
