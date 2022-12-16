087e9a9fef3c72f8e0ccac4e3cd06c1941ed8fe1
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-16 15:27:29 -0800

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	while (num <= 9)
 	{
-		if ((num != 2) || (num != 4))
+		if (num != 2 && num != 4)
 		{
 			_putchar(num + '0');
 			num++;
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 char _putchar(char c);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif
