b230f9926c730e2636eb5434edab9b0b189f7293
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-16 20:27:53 -0800

    Print numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (i = 0; i <= 14; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			num = j
 			if (j > 9)
+			{
 				_putchar('1');
+				num = j % 10;
+			}
 
-			_putchar((j % 10) + '0');
+			_putchar(num + '0');
 		}
 		_putchar('\n')
 	}
