874f3fc42968c2229d543d7389933cc7c84a8bbe
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-16 20:44:18 -0800

    Print numbers excluding 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	char num = 0;
+	int num = 0;
 
-	while (num <= 9)
+	for (; num <= 9; num++)
 	{
-		if (num != 2 && num != 4)
+		if (num == 2 && num == 4)
 		{
-			_putchar(num + '0');
-			num++;
+			continue;
 		}
+		_putchar(num + '0');
 	}
 	_putchar('\n');
 }
