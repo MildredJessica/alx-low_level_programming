88c5386567e48e9724e949e6b863e268a63b3d73
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-17 20:59:56 -0800

    Prints numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - Prints numbers from 0 to 14
- * Return: Nothing
+ * Return: 0
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
