d4490db32b6a69693f8f908c2239cac90c9ed403
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-20 11:15:15 -0800

    Prints the length of a string

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
+
 /**
  * main - Entry point
  * Return: 0
@@ -10,10 +12,16 @@ int main(void)
 	int k  = 49;
 	int *p = &n;
 	int *y = &k;
+	char *s;
+	
+	s = "Hello World";
 
 
-	/*reset_to_98(p);*/
+	/**
+	 * reset_to_98(p);
 	swap_int(y, p);
+	*/
+	printf("String length is %d\n", _strlen(s));
 
 	return (0);
 }
