bc53d19ceeefad8d3db3b78531cff173a197b2c6
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-20 19:10:58 -0800

    Prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,10 @@ void puts_half(char *str)
 	int len, p, i;
 
 	len = strlen(str);
-	if (len % 2 == 1)
-		 p = (len - 1) / 2;
-	else
+	if (len % 2 == 0)
 		p = len / 2;
+	else
+		p = (len - 1) / 2;
 	for (i = p ; i < len; i++)
 	{
 		_putchar(str[i]);
