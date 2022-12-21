ba5f268f09dcb2352b783133c5546d7786c6ec4b
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-21 01:34:30 -0800

    concantenates two string based on the value of n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	long int len, i;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
-	
+	for (len = 0; dest[len] != '\0'; len++)
+		continue;	
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
+		dest[len + i] = src[i];
 	
 	/*should end with a end of string char*/
-	dest[dest_len + i] = '\0';
+	dest[len + i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-	char *s = "Hell0 ";
-	char *y = "World";
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
 	char *ptr;
 
 	/**
 	 * s = _strcat(s, y);
-	 */
-	ptr = _strncat(s, y, 3);
+	 * ptr = _strcat(s, y)
+	 */;
+	ptr = _strncat(s1, s2, 3);
+	
 	printf("Conca %s\n", ptr);
 	return (0);
 }
