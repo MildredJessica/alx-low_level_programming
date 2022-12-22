c1a06370236df8a4492eec572b7001ab6c1e9319
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-21 22:24:39 -0800

    Converts a string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Parameter
+ * Return: Integer
+ */
+int _atoi(char *s)
+{
+	unsigned int num;
+
+	num = atoi(s);
+	return (num);
+}
