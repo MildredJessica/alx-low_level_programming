bf75259c7302205bf00ac885ae6e692ffe7be6a6
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-23 20:38:08 -0800

    Prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,15 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int k;
+
+	k = n;
+	if (k < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		k *= -1;
 	}
-	if ((n / 10) != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if ((k / 10) != 0)
+		print_number(k / 10);
+	_putchar((k % 10) + '0');
 }
