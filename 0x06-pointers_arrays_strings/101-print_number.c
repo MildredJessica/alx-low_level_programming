eb1ca1773f12045d2b71c8bc717a33c6eed15a0c
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-23 20:35:12 -0800

    Prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,7 +13,7 @@ void print_number(int n)
 		_putchar('-');
 		n *= -1;
 	}
-	if (n / 10)
+	if ((n / 10) != 0)
 		print_number(n / 10);
 	_putchar((n % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,36 +9,28 @@
  */
 char *cap_string(char *str)
 {
-	long int i, j = 0;
-	char sep[12] = {' ', ',', '.', ';', '!', '?', '"', '(', ')', '}', '{', ','};
+	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (i == 0)
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			i++;
+			if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+					str[i - 1] == '\n' ||
+					str[i - 1] == ',' ||
+					str[i - 1] == ';' ||
+					str[i - 1] == '.' ||
+					str[i - 1] == '!' ||
+					str[i - 1] == '?' ||
+					str[i - 1] == '"' ||
+					str[i - 1] == '(' ||
+					str[i - 1] == ')' ||
+					str[i - 1] == '{' ||
+					str[i - 1] == '}' ||
+					i == 0)
 				str[i] = str[i] - 32;
-			continue;
-		}
-		for (; sep[j] != '\0'; j++)
-		{
-			if (str[i] == sep[j])
-			{
-				++i;
-				if (str[i] >= 'a' && str[i] <= 'z')
-				{
-					str[i] = str[i] - 32;
-					break;
-				}
-			} else
-			{
-				if (str[i] == 'A' && str[i] == 'Z')
-				{
-					str[i] = str[i] + 32;
-					break;
-				}
-
-			}
+			i++;
 		}
 	}
 	return (str);
