25eab25a087c30cf150ab3da272156046dde4643
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2022-12-23 21:27:49 -0800

    Prints numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,36 @@
  */
 void print_number(int n)
 {
+	unsigned int d = 10, n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if ((n / 10) != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
