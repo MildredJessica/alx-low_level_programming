afbe0b7e5af39b9d4b20e65b0fe184beced8b546
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-04 09:25:10 -0800

    Changed the 2-strlen_recursion file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Prints the length of a string
+ * @s: Parameter
+ * Return: Length of a string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
-		return (1 + _strlen_recursion (s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	return (0);
 }
