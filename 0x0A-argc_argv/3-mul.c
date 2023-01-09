14aca89e529b75bbc08239e7ff5cab7947d2b149
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-08 19:13:51 -0800

    Multiplies the arguments passed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,14 @@ int main(int argc, char *argv[])
 	int i;
 	int multiply = 1;
 
-	if (argc <= 1)
-		printf("Error\n");
-	else
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			multiply *= atoi(argv[i]);
 		}
 		printf("%d\n", multiply);
-	}
+	} else
+		printf("Error\n")
 	return (1);
 }
