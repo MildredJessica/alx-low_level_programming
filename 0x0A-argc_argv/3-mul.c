5e5419910036cf18cf1cbbe71970f1f743838a84
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-14 23:08:18 -0800

    Changed the return value

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -23,5 +23,5 @@ int main(int argc, char *argv[])
 		printf("%d\n", multiply);
 	} else
 		printf("Error\n");
-	return (1);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Adds the arguments passed
+ * @argc: Argument counter
+ * @argv: One dimensional array
+ * Return: 1
+ */
 int main(int argc, char *argv[])
 {
+	int i;
+	int add = 0;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("Ehere%d\n", isalpha('j'));
+			if ((argv[i] >= 'A') && (argv[i] <= 'Z'))
+			{
+				printf("Error\n");
+				break;
+			}
+			else
+				add += atoi(argv[i]);
+
+			printf("%d\n", add);
+		}
+	}else 
+		printf("%d\n", add);
 	return (1);
 }
