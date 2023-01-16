2ed453a507799fc5ae9fe4978483b795ff1e7300
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-15 17:08:16 -0800

    Number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 {
 	int num = 0;
 	int count = 0;
+
 	if (argc <= 1)
 	{
 		printf("Error\n");
@@ -29,11 +30,11 @@ int main(int argc, char *argv[])
 		if (num - 25 >= 0)
 			num = num - 25;
 		else if (num - 10 >= 0)
-                        num = num - 10;
+			num = num - 10;
 		else if (num - 5 >= 0)
-                        num = num - 5;
+			num = num - 5;
 		else if (num - 2 >= 0)
-                        num = num - 2;
+			num = num - 2;
 		else if (num == 1)
 			num = num - 1;
 	}
