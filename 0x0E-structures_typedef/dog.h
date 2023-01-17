fe79ca3db1aa908edbd00acb4cf0a51f1a9d8713
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-16 18:24:50 -0800

    Changed the file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifdef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - Define a struct dog
+ * @name: Dog's name
+ * @age: Age
+ * @owner: owner's details
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}my_dog;
