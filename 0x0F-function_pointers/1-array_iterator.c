be29db288857a0b210882f9211d232f95fc68026
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-22 19:34:22 -0800

    A unction that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints element in the array
+ * @size: Size of the array
+ * @array: Array
+ * @action: The ponter to a function
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t count = 0;
+
+	if (size == 0 && action != NULL)
+		return;
+	while (count !=  size)
+	{
+		action(array[size]);
+		count++;
+	}
+}
