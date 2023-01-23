70d62acb78e9997542b05c62cbec6713f436eb31
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-22 19:39:25 -0800

    Changed a file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t count = 0;
 
-	if (size == 0 && action != NULL)
+	if (array == 0 && action != NULL)
 		return;
 	while (count !=  size)
 	{
-		action(array[size]);
+		action(array[count]);
 		count++;
 	}
 }
