be0f6c0e51f2e6cbd941d0a3009847fba4afd0f9
Mildred Jessica Nwonye <nwonyejessica@gmail.com>
2023-01-23 22:14:39 -0800

    multiplies two numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * stringtoint - convert a string to an integer.
+ * @str: char type string
+ * Return: integer converted
+ */
+
+int stringtoint(const char *str)
+{
+	int sign = 1;
+	unsigned long int resp = 0, num, i;
+
+	for (num = 0; !(str[num] >= 48 && str[num] <= 57); num++)
+	{
+		if (str[num] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = num; str[i] >= 48 && str[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (str[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+void print_int(unsigned long int n)
+{
+	unsigned  long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number pf argumnets
+ * @argv: Array of pointer
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	print_int(stringtoint(argv[1]) * stringtoint(argv[2]));
+	_putchar('\n');
+	return (0);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,10 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+int stringtoint(const char *str);
+void print_int(unsigned long int n);
+void _puts(char *str);
 
 #endif
