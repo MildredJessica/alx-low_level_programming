4e2e249569d2267165076b37b25669b0f2779310
MildredJessica <nwonyejessica@gmail.com>
2023-01-31 17:50:05 +0100

    Changed the 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,24 +10,35 @@
 listint_t *insert_nodeint_at_index(listint_t **head,
 		unsigned int idx, int n)
 {
-	listint_t *tmp = *head, *prev, *new_node;
+	listint_t *tmp = *head, *prev = tmp, *new_node;
 	unsigned int pos = 0;
 
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = NULL;
 	if (*head == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (tmp != NULL && pos != idx && tmp->next != NULL)
 	{
 		prev = tmp;
 		tmp = tmp->next;
 		pos++;
 	}
-	new_node = malloc(sizeof(listint_t));
-	new_node->n = n;
-	new_node->next = NULL;
 	if (pos == idx)
 	{
 		prev->next = new_node;
 		new_node->next = tmp;
+		tmp = new_node;
+		tmp->next = new_node->next;
 	} else
 		return (NULL);
 	return (new_node);
