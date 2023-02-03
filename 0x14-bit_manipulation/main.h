689e0fa9b9b6c6f1010a9e79a50ff9a06f476a9d
MildredJessica <nwonyejessica@gmail.com>
2023-02-04 00:24:26 +0100

    Prints the binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,5 +10,6 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
