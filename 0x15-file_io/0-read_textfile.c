2e0eeaad5ed444e4cbf1aeac24bed74c96a78580
MildredJessica <nwonyejessica@gmail.com>
2023-02-07 03:38:09 +0100

    Betty update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,29 +8,29 @@
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t num;
-    char *text;
-    int file;
+	ssize_t num;
+	char *text;
+	int file;
 
-    if (filename == NULL)
-	    return (0);
-    text = malloc(sizeof(char) * letters + 1);
-    if (text == NULL)
-	    return (0);
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-    {
-	    free(text);
-	    return(0);
-    }
-    num = read(file, text, sizeof(char) * letters);
-    if (num == -1)
-    {
-	    free(text);
-	    close(file);
-	    return (0);
-    }
-    free(text);
-    close(file);
-    return (num);
+	if (filename == NULL)
+		return (0);
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	num = read(file, text, sizeof(char) * letters);
+	if (num == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (num);
 }
