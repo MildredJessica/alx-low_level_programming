0e66a1ebc73e4740dd14e4e61f5c4df66ddee0c4
MildredJessica <nwonyejessica@gmail.com>
2023-03-16 22:27:34 +0100

    Adds a new node at the beginning of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
+ * @head: Pointer of a pointer to the linkedlist
+ * @n: The data to add
+ * Return: The address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new = malloc(sizeof(dlistint_t));
+
+if (new == NULL)
+{
+free(new);
+return (NULL);
+}
+new->n = n;
+new->prev = NULL;
+new->next = *head;
+*head = new;
+
+return (new);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -8,3 +8,9 @@ Write a function that prints all the elements of a dlistint_t list.
 Write a function that returns the number of elements in a linked dlistint_t list.
 
 - Prototype: size_t dlistint_len(const dlistint_t *h);
+
+## 2. Add node
+Write a function that adds a new node at the beginning of a dlistint_t list.
+
+- Prototype: dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+- Return: the address of the new element, or NULL if it failed
