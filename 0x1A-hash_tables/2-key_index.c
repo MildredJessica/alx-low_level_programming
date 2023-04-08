8bab74e353f5ee256af7de8cc51e4d0fb9235298
MildredJessica <nwonyejessica@gmail.com>
2023-04-08 04:39:52 +0100

    Applied for betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,11 +4,11 @@
  * key_index - Gives the index of  key
  * @key: Character to search for
  * @size: Size of the hashtable
- * Returns: The key at which the key os stored in the hastable
- */ 
+ * Return: The key at which the key is stored in the hastable
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-unsigned long index;
+unsigned long int index;
 
 index = hash_djb2(key) % size;
 return (index);
