287ca5fdb8cd3fbc44c9e64f5974a2e06f2fb0f1
MildredJessica <nwonyejessica@gmail.com>
2023-07-15 21:08:49 +0100

    Exponential, jump, advanced binary search algorithm

--- 0x1E-search_algorithms/tests/100-main.c
+++ 0x1E-search_algorithms/tests/100-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "search_algos.h"
+#include "../search_algos.h"
 
 /**
  * main - Entry point
@@ -10,12 +10,14 @@
 int main(void)
 {
     int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 47, 89, 160
     };
     size_t size = sizeof(array) / sizeof(array[0]);
+    printf("Size is: %lu\n", size);
 
-    printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
-    printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
-    printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
+
+    printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 50));
+    //printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
+    //printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
